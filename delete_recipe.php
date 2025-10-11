143a9db2260accc9408c8b044db55e2d58e5b875
weakbless098 <maryancrizaldo26@gmail.com>
2025-10-11 16:27:57 +0800

    VIEW AND DELETE DONE

---
+++ delete_recipe.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+  header("Location: login.php");
+  exit();
+}
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "user_management";
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+
+if (isset($_GET['id'])) {
+  $id = intval($_GET['id']);
+  $username = $_SESSION['username'];
+
+  // Check ownership
+  $check = $conn->prepare("SELECT image FROM recipes WHERE id = ? AND uploaded_by = ?");
+  $check->bind_param("is", $id, $username);
+  $check->execute();
+  $result = $check->get_result();
+
+  if ($result->num_rows > 0) {
+    $recipe = $result->fetch_assoc();
+    if (file_exists($recipe['image'])) unlink($recipe['image']);
+
+    $delete = $conn->prepare("DELETE FROM recipes WHERE id = ? AND uploaded_by = ?");
+    $delete->bind_param("is", $id, $username);
+    $delete->execute();
+  }
+}
+header("Location: my_recipe.php");
+exit();
+?>
